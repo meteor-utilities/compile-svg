f3bb46aed485d2214c93be25ac76b6995cb9a03c
Sacha Greif <sacha357@gmail.com>
2015-10-06 12:09:18 +0900

    init

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: "utilities:compile-svg",
+  summary: "Get SVG images and make them available as templates",
+  version: "0.1.0"
+});
+
+Package.registerBuildPlugin({
+  name: "compileSVG",
+  use: [
+    'caching-html-compiler',
+    'ecmascript',
+    'templating-tools',
+    'underscore'
+  ],
+  sources: ['lib/compile-svg.js'],
+  npmDependencies: {
+    "svg-sprite": "1.2.10"
+  }
+});
+
+Package.onUse(function (api) {
+
+  api.use('isobuild:compiler-plugin@1.0.0');
+
+});
