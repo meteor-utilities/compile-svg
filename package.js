253aeb7a2215e472c7b0f54a965ff80cbce77503
Sacha Greif <sacha357@gmail.com>
2015-10-09 16:05:32 +0900

    small tweaks

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: "utilities:compile-svg",
-  summary: "Get SVG images and make them available as templates",
-  version: "0.1.0"
+  summary: "Gather SVG files and make them available as a single sprite",
+  version: "0.1.1"
 });
 
 Package.registerBuildPlugin({
   name: "compileSVG",
   use: [
-    'caching-html-compiler',
-    'ecmascript',
-    'templating-tools',
-    'underscore'
+    'caching-html-compiler@1.0.1',
+    'ecmascript@0.1.4',
+    'templating-tools@1.0.0',
+    'underscore@1.0.4'
   ],
   sources: ['lib/compile-svg.js'],
   npmDependencies: {
@@ -21,7 +21,8 @@ Package.registerBuildPlugin({
 Package.onUse(function (api) {
 
   api.use([
-    'standard-app-packages',
+    'meteor-base@1.0.1',
+    'blaze-html-templates@1.0.1',
     'isobuild:compiler-plugin@1.0.0'
   ]);
 
